8cd7f2ebc4f6cf1aa54570852977141eed248018
Brischle.Fabio <Brischle.Fabio@SK-D43D7EB3B863.2631.skool.net>
2016-09-22 08:16:54 +0200

    fiecken sie sich

--- helloworld/src/helloworld/helloworld.java
+++ helloworld/src/helloworld/helloworld.java
@@ -1,9 +1,17 @@
 package helloworld;
 
 public class helloworld {
-
+		static double z1 = 523	;
+		static double z2 = 4535;
+		static double z3 = 0;
+		
 	public static void main(String[] args) {
-		System.out.println("Hello World  !"); 	//jhghghnfj
+		System.out.println("Hello World  !"); 	
+		
+		z3=z1/z2;
+		
+		System.out.println(z3);
+		
 
 	}
 
